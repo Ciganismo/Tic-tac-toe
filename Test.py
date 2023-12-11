5c14b5e5c3ea2d3fea76890ef222115d8b3bd181
Ciganismo <dp.maksimov@yandex.ru>
2023-12-11 18:09:41 +0100

    BASE

---
+++ Test.py
@@ -0,0 +1,4 @@
+r = int(input())
+rt = [2,1]
+if (r in range(0, 3)) and (r not in rt):
+    print(True)
--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,5 +1,6 @@
 seq_num_move = ("Первый", "Второй", "Третий", "Четвёртый", "Пятый", "Шестой", "Седьмой", "Восьмой", "Девятый")
-
+filled_columns = []
+filled_rows = []
 board = [[' ', '0', '1', '2'],
          ['0', '-', '-', '-'],
          ['1', '-', '-', '-'],
@@ -25,7 +26,7 @@ num_of_moves = 1
 game_over = False
 
 # start game
-while (game_over != True) and (num_of_moves < 10):
+while (not game_over) and (num_of_moves < 10):
     print(f"{seq_num_move[num_of_moves - 1]} ход")
     num_of_moves += 1
     print_board(board)
@@ -34,5 +35,11 @@ while (game_over != True) and (num_of_moves < 10):
     else:
         print("Ходят нолики")
     while True:
-        pass
-        break
+        player_row = int(input("Введите номер строки(0,1,2):"))
+        player_column = int(input("Введите номер столбца(0,1,2):"))
+        if ((player_row in range(0, 3)) and (player_row not in filled_rows)) \
+                and ((player_column in range(0, 3)) and (player_column not in filled_columns)):
+            filled_rows.append(player_row)
+            filled_columns.append(player_column)
+        else:
+            print(False)
